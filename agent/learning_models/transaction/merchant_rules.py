facb06641cc6904b7c6aba669c740133a24cf19e
jerrylhsieh-svg <jerry.l.hsieh1992@gmail.com>
2026-05-01 15:49:57 -0400

    rules

--- agent/learning_models/transaction/merchant_rules.py
+++ agent/learning_models/transaction/merchant_rules.py
@@ -7,29 +7,14 @@ from agent.models.merchant_prediction import MerchantRule
 
 OVERRIDE_RULES: list[MerchantRule] = [
     MerchantRule(
-        re.compile(r"\b(uber\s*one|uber\s*pass)\b", re.I),
-        "subscription_membership",
-        "Uber membership should not be classified as rideshare.",
-    ),
-    MerchantRule(
         re.compile(r"\b(uber\s*eats|ubereats|doordash|grubhub|seamless)\b", re.I),
-        "food_delivery",
-        "Food delivery should be separated from restaurants.",
-    ),
-    MerchantRule(
-        re.compile(r"\b(uber\s*trip|uber\*?\s*trip|lyft)\b", re.I),
-        "transportation",
-        "Rideshare should be separated from Uber Eats and Uber One.",
-    ),
-    MerchantRule(
-        re.compile(r"\b(amazon\s*prime|amzn\s*prime|prime\s*membership)\b", re.I),
-        "subscription_membership",
-        "Amazon subscription should be separated from Amazon shopping.",
+        "Dining",
+        "Food delivery should be Dining",
     ),
     MerchantRule(
         re.compile(r"\b(amazon|amzn)\b", re.I),
-        "amazon",
-        "Amazon purchases should have their own category.",
+        "Merchandise",
+        "Amazon purchases should be Merchandise.",
     ),
 ]
 
