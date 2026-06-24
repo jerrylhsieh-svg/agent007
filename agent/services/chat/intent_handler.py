c950d9a462a438ad3cb21550d36532ccafe541f4
jerrylhsieh-svg <jerry.l.hsieh1992@gmail.com>
2026-06-24 11:28:52 -0400

    query_executor

--- agent/services/chat/intent_handler.py
+++ agent/services/chat/intent_handler.py
@@ -11,6 +11,8 @@ from agent.services.train_models_service import train_model
 
 Handler = Callable[..., str]
 
+query_executor = QueryExecutor()
+
 INTENT_HANDLERS: dict[Intents, tuple[Handler, dict[str, Any]]] = {
     Intents.ANALYZE_CARD_TRANSACTIONS: (generate_credit_card_summary,{}),
     Intents.ANALYZE_BANK_STATEMENTS: (generate_bank_statement_summary,{}),
@@ -18,7 +20,7 @@ INTENT_HANDLERS: dict[Intents, tuple[Handler, dict[str, Any]]] = {
     Intents.TRAIN_STATEMENT_MODEL: (train_model,{"file_type": "statement"}),
     Intents.REPREDICT_STATEMENT_RECORDS: (repredict_records, {"file_type": "statement"}),
     Intents.REPREDICT_CARD_RECORDS: (repredict_records, {"file_type": "transaction"}),
-    Intents.QUERY_TRANSACTIONS: (QueryExecutor.executing, {}),
+    Intents.QUERY_TRANSACTIONS: (query_executor.executing, {}),
     Intents.LABEL_RECORDS: (handle_label_flow, {}),
     Intents.GENERAL_CHAT: (call_model, {"label_sessions": label_sessions}),
     Intents.UNKNOWN: (call_model, {"label_sessions": label_sessions}),
